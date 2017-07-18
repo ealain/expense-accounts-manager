f7875e6c29afe4e29a41ca78eed26cda11253150
Éloi Alain <eloi.alain@supelec.fr>
2017-07-18 16:25:56 +0200

    Fix admin logout

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../auth/auth.service';
+
 @Component({
     templateUrl: './admin.component.html'
 })
 export class AdminComponent {
     constructor(
+        private authService: AuthService,
         private router: Router) {}
 
     home(): void {
@@ -14,5 +17,14 @@ export class AdminComponent {
 
     adduser(): void {
         this.router.navigate(['adduser']);
+    }
+
+    logout(): void {
+        this.authService.logout()
+        .then(response => {
+            if(response.success) {
+                this.router.navigate(['login']);
+            }
+        });
     }
 }
