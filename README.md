33c869e1565c817a0b923be1c8105e3fc7a89e4c
Éloi Alain <eloi.alain@supelec.fr>
2017-07-20 13:56:40 +0200

    Update README and install scripts

--- README.md
+++ README.md
@@ -10,15 +10,25 @@ Download the application code:
 ```
 git clone https://github.com/ealain/expense-accounts-manager.git
 ```
+
+### Development installation
+
+Install and configure the application (Bash script):
+```
+cd expense-accounts-manager
+./install-dev.sh
+```
+By default, a directory *data* will be created for MongoDB. Another directory *log* will be created to store more information about the installation process. The latter may be deleted.
+
+Follow the instructions that appear at the end of the output.
+
+### Production installation
+
 Install and configure the application (Bash script):
 ```
 cd expense-accounts-manager
 ./install.sh
 ```
-By default, a directory *data* will be created for MongoDB. Another directory *log* will be created to store more information about the installation process. The latter may be deleted.
+By default, a directory *data* will be created for MongoDB.
 
-Run the application:
-```
-mongod [--nojournal] --dbpath=data
-node index.js
-```
+Follow the instructions that appear at the end of the output.
--- install.sh
+++ install.sh
@@ -23,32 +23,28 @@ printf "\n}" >> config.js
 
 # Starting local database
 printf "\nStarting local database at localhost:27017/ea-manager..."
-mkdir log 2> /dev/null
 mkdir data 2> /dev/null
 mkdir data/uploads 2> /dev/null
-echo -n "" > log/db.log
-echo -n "" > log/install.log
-mongod --nojournal --dbpath=data 1>> log/db.log 2>> log/db.log &
+mongod --nojournal --dbpath=data 1>> /dev/null 2>> /dev/null &
 MONGO_PID="$!"
 
 # Fetching NodeJS modules (also waiting for mongod to start)
 printf "\nInstalling Node modules..."
-npm install &> /dev/null
+npm install --only=production &> /dev/null
 
 # Transpiling TS into JS (Angular app)
 printf "\nTranspiling front-end..."
-ng build &> /dev/null
+ng build --prod &> /dev/null
 
 # Admin creation
 printf "\nCreating first user (admin)"
-mongo ea-manager --eval "db.users.insertOne({login: \"$login\", password: \"$password\", admin: true, manager: false})" 1>> log/install.log 2>> log/install.log
+mongo ea-manager --eval "db.users.insertOne({login: \"$login\", password: \"$password\", admin: true, manager: false})" 1>> /dev/null 2>> /dev/null
 
 if [ -z "$address_db" ]; then
-    kill $MONGO_PID 2>> log/install.log
-    echo "Database directory has been set to './data'" >> log/install.log
+    kill $MONGO_PID 2>> /dev/null
+    echo "Database directory has been set to './data'" >> /dev/null
 fi
 
-printf "\nMore information on the installation is available under log/\n"
 printf "\nTo start the application run\n"
-printf "\t- db_server.sh\n"
+printf "\t- db_server.sh (mongo daemon)\n"
 printf "\t- node index.js\n"
